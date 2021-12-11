15fe71c90d070f016031be7c9928de67c73bd2de
volhashax <volhashax@gmail.com>
2021-12-11 23:30:43 +0300

    massive is returned

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,39 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] m = ArrayGeneration(n);
+            
+            for(int i = 0; i < m.Length; i++)
+            { 
+            
+                if (m[i]% 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (m[i] % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (m[i] % 3 == 0 && m[i] % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+           }
+
+        }
+
+        private static int[] ArrayGeneration(int n)
+        {
+            int[] array = new int[n];
+
+            for(int i = 1; i <= array.Length; i++)
+            {
+                array[i-1] = i;
+            }
+
+            return array;
         }
     }
 }
