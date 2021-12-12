8876046ac227d3f4ba0d005bf4c02f3e8c7eb8eb
volhashax <volhashax@gmail.com>
2021-12-12 11:14:28 +0300

    algorithm update

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -11,21 +11,28 @@ namespace FizzBuzz
             int[] m = ArrayGeneration(n);
             
             for(int i = 0; i < m.Length; i++)
-            { 
-            
-                if (m[i]% 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                if (m[i] % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
+            {
                 if (m[i] % 3 == 0 && m[i] % 5 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
+                    Console.Write("FizzBuzz");
                 }
-           }
+                else if (m[i]% 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if (m[i] % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else
+                {
+                    Console.Write(m[i]);
+                }
+                if (i != m.Length-1)
+                { 
+                    Console.Write(", ");
+                }
+            }
 
         }
 
